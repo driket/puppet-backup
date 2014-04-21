bfde9f973ef4d34bf543e80e9b600269cbec4b5d
Cedric Villa <cedric.villa@gmail.com>
2014-04-21 15:52:33 +0200

    lint

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,12 +1,12 @@
 class mv_backup::config {
-	require('mv_backup::packages')
-	
-	file { "${mv_backup::config_path}/mv_backup":
-  	ensure 	=> directory,
-    mode 		=> "0755",
-    owner 	=> 'root',
-    group 	=> 'root',
-		require => Package['lftp','rsync'],
+  require('mv_backup::packages')
+  
+  file { "${mv_backup::config_path}/mv_backup":
+    ensure  => directory,
+    mode    => "0755",
+    owner   => 'root',
+    group   => 'root',
+    require => Package['lftp','rsync'],
   }
-			
+      
 }
